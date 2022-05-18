b9381680507e0ca0112c050c4d962a4c00acded2
Ed Scheinerman <ers@jhu.edu>
2022-05-18 17:01:14 -0400

    MInor clean up & speedup

--- src/bisection.jl
+++ src/bisection.jl
@@ -3,16 +3,17 @@ export bisection_solve
     bisection_solve(f::Function, a::Real, b::Real, tol::Real=1e-8)
 Solve the equation `f(x)=0` in the interval `[a,b]`. We require `f(a)*f(b) <= 0`.
 """
-function bisection_solve(f::Function, a::Real, b::Real, tol::Real=1e-8)
+function bisection_solve(f::Function, a::Real, b::Real, tol::Real = 1e-8)
     if tol <= 0
         error("Tolerance must be positive")
     end
 
     fa = f(a)
-    fb = f(b)
     if abs(fa) <= tol
         return a
     end
+
+    fb = f(b)
     if abs(fb) <= tol
         return b
     end
@@ -22,21 +23,19 @@ function bisection_solve(f::Function, a::Real, b::Real, tol::Real=1e-8)
     end
 
     while true
-        c = (a+b)/2
+        c = (a + b) / 2
         fc = f(c)
         if abs(f(c)) <= tol
-            return c 
+            return c
         end
 
         if sign(fc) != sign(f(b))
             a = c
-            fa = fc 
+            fa = fc
         else
             b = c
-            fb = fc 
+            fb = fc
         end
     end
-
-
 
 end
